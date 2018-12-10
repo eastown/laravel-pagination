02570c62dfc8c3f7ff936d1a83b12e42bd0f49a1
xiaoqi <xiaoqi@mopgaming.info>
2018-12-10 18:28:43 +0800

    sum return data filter

--- src/Pagination.php
+++ src/Pagination.php
@@ -113,7 +113,7 @@ class Pagination
         $raw = $this->raw(join(',', array_map(function($field){
             return "SUM({$field}) AS {$field}";
         }, $fields)));
-        return (clone $this->builder)->select($raw)->first();
+        return array_only((clone $this->builder)->select($raw)->first()->toArray(), $fields);
     }
 
     public function query()
